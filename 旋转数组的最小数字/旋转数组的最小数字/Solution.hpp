806e740e3c58a7ae377038ecec6eae0848c04c60
Traim304 <Traim304@163.com>
2016-06-30 17:39:24 +0800

    基本功能实现

--- 旋转数组的最小数字/旋转数组的最小数字/Solution.hpp
+++ 旋转数组的最小数字/旋转数组的最小数字/Solution.hpp
@@ -3,4 +3,14 @@
 
 #include <stdio.h>
 
+class Solution
+{
+    int *array;
+    int m_length;
+public:
+    Solution();
+    int minimum(int min_flag,int max_flag);
+    int get_length();
+};
+
 #endif /* Solution_hpp */
--- 旋转数组的最小数字/旋转数组的最小数字/main.cpp
+++ 旋转数组的最小数字/旋转数组的最小数字/main.cpp
@@ -3,9 +3,15 @@
 //例如数组 {3,4,5,1,2} 为 {1,2,3,4,5} 的一个旋转，该数组的最小值为 1。
 //NOTE：给出的所有元素都大于 0，若数组大小为 0，请返回 0。
 #include <iostream>
-
+#include "Solution.hpp"
+using std::cout;
+using std::endl;
 int main(int argc, const char * argv[]) {
-    
+    Solution slt;
+    int save_minimum;
+    //注意数组越界
+    save_minimum = slt.minimum(0,slt.get_length() - 1);
+    cout << "Minimum = " << save_minimum << endl;
     return 0;
 }
 
