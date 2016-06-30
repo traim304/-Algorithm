d08eac9ecf56915b3add36fa22120c45c6a893bf
Traim304 <Traim304@163.com>
2016-06-30 17:47:26 +0800

    init

---
+++ 斐波那契数列/斐波那契数列/main.cpp
@@ -0,0 +1,11 @@
+//斐波纳契数列（Fibonacci Sequence），又称黄金分割数列。
+//在数学上，斐波纳契数列以如下被以递归的方法定义：
+//F(0)=0，
+//F(1)=1，
+//F(n)=F(n-1)+F(n-2)
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
