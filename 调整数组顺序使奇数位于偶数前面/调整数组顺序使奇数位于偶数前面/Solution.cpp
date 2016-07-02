1016a45523336d7ae74d6db4a173df2824a83972
Traim304 <Traim304@163.com>
2016-07-02 19:08:30 +0800

    init  调整数组顺序使奇数位于偶数前面

---
+++ 调整数组顺序使奇数位于偶数前面/调整数组顺序使奇数位于偶数前面/Solution.cpp
@@ -0,0 +1,14 @@
+//
+//  Solution.cpp
+//  调整数组顺序使奇数位于偶数前面
+//
+//  Created by JacobLan on 16/7/2.
+//  Copyright © 2016年 JacobLan. All rights reserved.
+//
+
+#include "Solution.hpp"
+
+void Solution::re_order_array(int *array)
+{
+    
+}
---
+++ 调整数组顺序使奇数位于偶数前面/调整数组顺序使奇数位于偶数前面/Solution.hpp
@@ -0,0 +1,20 @@
+//
+//  Solution.hpp
+//  调整数组顺序使奇数位于偶数前面
+//
+//  Created by JacobLan on 16/7/2.
+//  Copyright © 2016年 JacobLan. All rights reserved.
+//
+
+#ifndef Solution_hpp
+#define Solution_hpp
+
+#include <stdio.h>
+
+class Solution
+{
+public:
+    void re_order_array(int* array);
+};
+
+#endif /* Solution_hpp */
